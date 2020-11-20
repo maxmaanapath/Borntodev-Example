610c13ecec7471c10fc222d7e4c8b70e42530811
GotItMyDADDY <maxmaanapath@gmail.com>
2020-11-21 01:54:21 +0700

    Add Lecture16
    show type() command

---
+++ Lecture16.py
@@ -0,0 +1,7 @@
+print(type("Hello World"))
+print(type(10))       # class 'int'
+print(type(1.5))      # class 'float'
+print(type(True))     # class 'bool'
+print(type(1j))       # class 'complex'
+print(10)
+print("Hello World")
