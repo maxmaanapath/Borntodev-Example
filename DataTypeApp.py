fe2e5031f93cf60c82734ba79951263159b92d03
GotItMyDADDY <maxmaanapath@gmail.com>
2020-11-27 10:49:43 +0700

    Create DataTypeApp
    Create Lecture from Borntodev

---
+++ DataTypeApp.py
@@ -0,0 +1,13 @@
+print("Welcome to Magma Shop")
+print("---------------------")
+print("Apple", 3, 30,"THB =",3*30, "THB")
+print("Banana", 1, 10,"THB =",1*10, "THB")
+print("Coffee", 1, 50,"THB =",1*50, "THB")
+print("Tea" , 1, 60, "THB =",1*60, "THB")
+print("Cake", 2, 60, "THB =",2*60, "THB")
+print("Milk", 1, 30, "THB =",1*30, "THB")
+print("---------------------")
+print("Total", (3*30)+(1*10)+(1*50)+(1*60)+(2*60)+(1*30), "THB")
+print("Thank you : Magma Shop"
+
+print("10"+"5")
