13cca72504bbe200ba0a8013e01253e643b15a67
GotItMyDADDY <maxmaanapath@gmail.com>
2020-12-01 22:01:00 +0700

    Create Lecture24
    Create Variable example

---
+++ Lecture24_VariableExample.py
@@ -0,0 +1,8 @@
+Name = "Napath Samleepant"
+Ages = 27
+Height = 171
+Weight = 69
+print("Hello!! My Name is", Name)
+print("I'm",Ages,"years old")
+print("Height",Height,"cm")
+print("Weight",Weight,"kg")
