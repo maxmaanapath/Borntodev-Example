4d4fa3e61fd03eab60cb535b724812a1038b869b
GotItMyDADDY <maxmaanapath@gmail.com>
2020-12-02 21:03:21 +0700

    Create ElifExample
    Create lecture about Elif condition

---
+++ ElifExample.py
@@ -0,0 +1,11 @@
+if False:
+    print("First Condition")
+    print("First Condition")
+    print("First Condition")
+    print("First Condition")
+elif True:
+    print("Second Condition")
+elif False:
+    print("Third Condition")
+else:
+    print("Else Condition")
