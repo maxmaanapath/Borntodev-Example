c83b5d011c3444bd4254df7ce95290dc8a995dec
GotItMyDADDY <maxmaanapath@gmail.com>
2020-12-02 20:59:47 +0700

    Create Login Example
    Create login example

---
+++ LoginExample.py
@@ -0,0 +1,8 @@
+usernameInput = input("Username : ")
+passwordInput = input("Password : ")
+
+if usernameInput == "admin" and passwordInput == "1234":
+    print("Done!!")
+else:
+    print("!!!Wrong Username or Password!!!")
+    print("Please Log in again")
