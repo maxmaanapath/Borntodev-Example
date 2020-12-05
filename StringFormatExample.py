22915ecb4804d8061f3dfc3cdd3311d05ed2da9f
GotItMyDADDY <maxmaanapath@gmail.com>
2020-12-05 16:22:18 +0700

    Create StringFormatExample
    Create String format example

---
+++ StringFormatExample.py
@@ -0,0 +1,5 @@
+name = "Napath"
+weight = "61"
+print("Hello "+name+" and weight is "+weight+"kg")
+print("Hello %s and weight is %dkg" % (name,66))
+print("Hello %s and weight is %fkg" % (name,66.66))
