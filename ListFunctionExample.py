90202d20c5aa31d9f6cb9ae029fe667da30bb550
GotItMyDADDY <maxmaanapath@gmail.com>
2020-12-06 01:49:34 +0700

    Create ListFunctionExample
    Create List function example

---
+++ ListFunctionExample.py
@@ -0,0 +1,9 @@
+myFriends = ["Magma","SomO","Tangmo"]
+print(myFriends)
+myFriends[1] = "Som"
+myFriends.append("Bank")
+print(myFriends)
+myFriends.remove("Tangmo")
+print(myFriends)
+del myFriends[0]
+print(myFriends)
