e151fc1d46346ec6635c2adfca39f54658358d79
GotItMyDADDY <maxmaanapath@gmail.com>
2020-12-06 01:52:42 +0700

    Create TupleFunctionExample
    Create Tuple function example

---
+++ TupleFunctionExample.py
@@ -0,0 +1,9 @@
+tupleEx = (1,200,3,4,10)
+listEx = [1,200,3,4,10]
+print(len(listEx))
+print(max(listEx))
+print(min(listEx))
+#edit list to tuple
+print(tuple(listEx))
+#find first index of value
+print(tupleEx.index(200))
