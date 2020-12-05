29b80cbd8dd96c862e312b9982ff01102288785b
GotItMyDADDY <maxmaanapath@gmail.com>
2020-12-06 01:45:28 +0700

    Create StringFunctionExample
    Create String function example

---
+++ StringFunctionExample.py
@@ -0,0 +1,11 @@
+name = input("Firstname : ").capitalize()
+lastname = input("Lastname : ").capitalize()
+print("Hello !! %s %s !"%(name,lastname))
+
+text = "Magma"
+textFormatted = "Welcome %s"%(text)
+print(textFormatted.center(21,"-"))
+
+text = "Magma"
+textFormatted = "Welcome %s"%(name)
+print(len(name))
