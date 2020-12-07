25e6339765ec94b0dae76b1e7ec8b1945ab8e84e
GotItMyDADDY <maxmaanapath@gmail.com>
2020-12-07 10:25:36 +0700

    Create DictionaryExample
    Create Dictionary example

---
+++ DictionaryExample.py
@@ -0,0 +1,6 @@
+dict1 = {"Name":"Magma","Age":27}
+print(dict1["Name"],dict1["Age"])
+dict1["Lastname"] = "Samleepant"
+print("Hello",dict1["Name"],dict1["Lastname"])
+dict1["Name"] = "Napath"
+print(dict1)
