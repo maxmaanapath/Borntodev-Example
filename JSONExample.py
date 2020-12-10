5a2dfc1bc7618b219386ed423cbccd597819a0a7
GotItMyDADDY <maxmaanapath@gmail.com>
2020-12-10 17:23:19 +0700

    Create JSONExample
    Create JSON Example

---
+++ JSONExample.py
@@ -0,0 +1,25 @@
+import json
+def readJson():
+    # some JSON:
+    x =  '{ "name":"John", "age":30, "city":"New York"}'
+
+    # parse x:
+    y = json.loads(x)
+
+    # the result is a Python dictionary:
+    print(y["name"])
+def writeJson():
+    # a Python object (dict):
+    x = {
+        "name": "John",
+        "age": 30,
+        "city": "New York"
+    }
+
+    # convert into JSON:
+    y = json.dumps(x)
+
+    # the result is a JSON string:
+    print(y)
+readJson()
+writeJson()
