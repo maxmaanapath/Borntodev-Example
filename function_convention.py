282107dd96e7c216c7652ccae24d9e3882b95a5a
GotItMyDADDY <maxmaanapath@gmail.com>
2020-12-12 16:16:21 +0700

    Create function_convention
    Create Function Convention example

---
+++ function_convention.py
@@ -0,0 +1,9 @@
+def print_hello_world():
+    print("Hello World")
+    print("Woooooooo!!")
+def print_hello():
+    print("Hello Napath")
+    print("Hahaa")
+    print_hello_world()
+print_hello_world()
+print_hello()
