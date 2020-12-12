709cfdffb9f6b3521b1fa94e4e06fb404ad3dc88
GotItMyDADDY <maxmaanapath@gmail.com>
2020-12-12 16:09:16 +0700

    Create InformationHiding
    Create Information Hiding example

---
+++ Lecture107_overridingExample - Copy.py
@@ -0,0 +1,16 @@
+class Animal():
+    def eat(self):
+        print("Eating Eating!")
+
+class Cat(Animal):
+    __name = ""
+    def setName(self,text):
+        self.name = text
+        print("Setting New Cat Name",self.name)
+    def eat(self):
+        print("Meoww !!",self.name)
+
+cat1 = Cat()
+cat1.setName("TT")
+print(cat1.__name)
+cat1.eat()
